35913085c69ee90f550af4726ccfb9861ae46006
xubitao <766191920@qq.com>
2016-01-06 23:56:12 +0800

    优化内部结构,增加对pathVariables的支持

--- src/main/java/cn/xubitao/dolphin/foundation/response/Response.java
+++ src/main/java/cn/xubitao/dolphin/foundation/response/Response.java
@@ -1,6 +1,8 @@
 package cn.xubitao.dolphin.foundation.response;
 
-import cn.xubitao.dolphin.foundation.resource.RestResource;
+import cn.xubitao.dolphin.foundation.assmbler.DolphinAssembler;
+import cn.xubitao.dolphin.foundation.exceptions.ClientException;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.http.HttpEntity;
@@ -30,10 +32,23 @@ public class Response {
         this.httpStatus = httpStatus;
     }
 
-    public static HttpEntity<ResourceSupport> build(Object domain, ResourceAssemblerSupport resourceAssembler) {
+    public static HttpEntity<ResourceSupport> build(Object domain, DolphinAssembler resourceAssembler) {
         Response response = new Response();
-        ResourceSupport domainResource = resourceAssembler.toResource(domain);
+        ResourceSupport domainResource = resourceAssembler.toResource(domain, new Integer[0]);
         ResourceSupport resource = new ResourceSupport();
+        setResponse(domain, response, domainResource, resource);
+        return new ResponseEntity<ResourceSupport>(response.getRestResource(), response.getHttpStatus());
+    }
+
+    public static HttpEntity<ResourceSupport> build(Object domain, DolphinAssembler resourceAssembler, Integer... pathVariables) {
+        Response response = new Response();
+        ResourceSupport domainResource = resourceAssembler.toResource(domain, pathVariables);
+        ResourceSupport resource = new ResourceSupport();
+        setResponse(domain, response, domainResource, resource);
+        return new ResponseEntity<ResourceSupport>(response.getRestResource(), response.getHttpStatus());
+    }
+
+    private static void setResponse(Object domain, Response response, ResourceSupport domainResource, ResourceSupport resource) {
         if (domain == null) {
             response.setHttpStatus(HttpStatus.NOT_FOUND);
             resource.add(domainResource.getLinks());
@@ -41,7 +56,6 @@ public class Response {
         } else {
             response.setRestResource(domainResource);
         }
-        return new ResponseEntity<ResourceSupport>(response.getRestResource(), response.getHttpStatus());
     }
 
     public static ResponseEntity ok() {
@@ -54,5 +68,11 @@ public class Response {
 
     public static ResponseEntity created(ResourceSupport resourceSupport) {
         return new ResponseEntity(resourceSupport, HttpStatus.CREATED);
+    }
+
+    public static JSONObject error(Exception exception) {
+        JSONObject message = new JSONObject();
+        message.put("message", exception.getMessage());
+        return message;
     }
 }
